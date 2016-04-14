ec22bdbcd11d3ed71d6dd9e217cfea5b14b7a19a
Keiko Fujii <kfujii12@georgefox.edu>
2016-04-13 18:13:20 -0700

    This is a slightly more intelligent player. Still beats all the dumb players.

--- PrologClient.pl
+++ PrologClient.pl
@@ -14,10 +14,12 @@
 %
 % Author:   1668650
 % Written:  3/29/05
-% Version:  1.0
+% Version:  2.0 
 %
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Hunches? 
 
 % Identify which rules we can assert and retract dynamically
 :- dynamic adjacent/2.
@@ -39,6 +41,9 @@
 :- dynamic gamesWon/2.
 :- dynamic totalGames/1.
 :- dynamic deducedHasCard/2. 
+:- dynamic isSusAnswer/1.
+:- dynamic isWeapAnswer/1.
+:- dynamic isRoomAnswer/1.
 
 % A rule to use instead of asserta to prevent reasserting the same fact 
 % Deduces hasCard at the end of adding the new fact
@@ -55,7 +60,8 @@ deduceHasCard :-
   cantHave(Player, Weapon), 
   cantHave(Player, Room), 
   \+ deducedHasCard(Player, Suspect), % See if the fact already is known
-  addNewFact(deducedHasCard(Player, Suspect)).
+  addNewFact(deducedHasCard(Player, Suspect)), writeLog(['DHS1']),
+  isSusAnswer(_).
 
 % Checks to see if we can deduce that they knew the weapon
 deduceHasCard :- 
@@ -63,7 +69,8 @@ deduceHasCard :-
   cantHave(Player, Suspect), 
   cantHave(Player, Room), 
   \+ deducedHasCard(Player, Weapon), % See if the fact already is known
-  addNewFact(deducedHasCard(Player, Weapon)).
+  addNewFact(deducedHasCard(Player, Weapon)), writeLog(['DHS2']),
+  isWeapAnswer(_).
 
 % Checks to see if we can deduce that they knew the room
 deduceHasCard :- 
@@ -71,7 +78,8 @@ deduceHasCard :-
   cantHave(Player, Weapon), 
   cantHave(Player, Suspect), 
   \+ deducedHasCard(Player, Room), % See if the fact already is known
-  addNewFact(deducedHasCard(Player, Room)).
+  addNewFact(deducedHasCard(Player, Room)), writeLog(['DHS3']),
+  isRoomAnswer(_).
 
 % Base case, just pass
 deduceHasCard.
@@ -100,7 +108,10 @@ reset :-
    retractall(suggested(_,_,_,_)),
    retractall(didntRefute(_,_,_,_)),
    retractall(accused(_,_,_,_)),
-   retractall(deducedHasCard(_,_)).
+   retractall(deducedHasCard(_,_)),
+   retractall(isSusAnswer(_)),
+   retractall(isWeapAnswer(_)),
+   retractall(isRoomAnswer(_)).
 
 
 
@@ -124,6 +135,22 @@ init(Me, StartRoom, Suspects, Weapons, RoomLists, Cards, ActivePlayers) :-
 initDefault(Me,StartRoom, MyCards) :-
    Suspects = [missscarlet,mrgreen,mrswhite,professorplum,mrspeacock,
    colonelmustard],
+   Weapons = [rope,leadpipe,wrench,candlestick,knife,revolver],
+   Rooms = [[hall,[study,lounge]],
+       [study,[hall,library,kitchen]],
+       [library,[billiardroom,study]],
+       [billiardroom,[library,conservatory]],
+       [conservatory,[billiardroom,lounge,ballroom]],
+       [ballroom,[conservatory,kitchen]],
+       [kitchen,[diningroom,study,ballroom]],
+       [diningroom,[kitchen,lounge]],
+       [lounge,[diningroom,hall,conservatory]]],
+   % Assume everyone is active
+   init(Me, StartRoom, Suspects, Weapons, Rooms, MyCards, Suspects).
+
+% Init a game with default configuration (could use this to play
+% interactively)
+initDefault(Me, StartRoom, MyCards, Suspects) :-
    Weapons = [rope,leadpipe,wrench,candlestick,knife,revolver],
    Rooms = [[hall,[study,lounge]],
        [study,[hall,library,kitchen]],
@@ -172,7 +199,7 @@ showsMe(Player, Card) :- addNewFact(showedMe(Player, Card)).
 
 
 % Suggestions from other players- moves suspects to new rooms
-% and player must be in the room as well - might want to remember...
+% and player must be in the room as well 
 suggests(Suspect, Weapon, Room, Player) :- 
   moveTo(Player,Room),
   moveTo(Suspect,Room),
@@ -184,13 +211,11 @@ suggests(Suspect, Weapon, Room, Player) :-
 refutes(Suspect, Weapon, Room, Player) :- 
   addNewFact(refuted(Suspect, Weapon, Room, Player)).
 
-% Failed refutations I have overheard - might want to remember...
-% Added
+% Failed refutations I have overheard
 cantRefute(Suspect, Weapon, Room, Player) :-
   addNewFact(didntRefute(Suspect, Weapon, Room, Player)).
 
-% Failed accusations I have overheard - might want to
-% remember...
+% Failed accusations I have overheard
 accuse(Suspect, Weapon, Room, Player) :- 
   addNewFact(accused(Suspect, Weapon, Room, Player)).
 
@@ -198,9 +223,6 @@ accuse(Suspect, Weapon, Room, Player) :-
 moveTo(Suspect, Room) :- 
    retractall(isIn(Suspect, _OldRoom)), 
    addNewFact(isIn(Suspect, Room)). 
-
-
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -229,11 +251,18 @@ answer(Suspect, Weapon, Room, WinningPlayer) :-
    forall(activePlayer(P), printPoints(P)), nl,nl,nl.
 
 
-% Refute a suggestion with a suspect, weapon, or room. Might want to be
-% smarter about this...
-refute(Suspect, _W, _R, _A, Suspect) :- iHave(Suspect).
-refute(_S, Weapon, _R, _A, Weapon) :- iHave(Weapon).
-refute(_S, _W, Room, _A, Room) :- iHave(Room).
+% Refute a suggestion with a suspect, weapon, or room. 
+% If something has been refuted already, then refute it again
+% Otherwise pick a card that we can refute
+refute(Suspect, _, _, _, Suspect) :- 
+    refuted(Suspect, _, _, _), iHave(Suspect), writeLog(['R1']).
+refute(_, Weapon, _, _, Weapon) :- 
+    refuted(_, Weapon, _, _), iHave(Weapon), writeLog(['R2']).
+refute(_, _, Room, _, Room) :- 
+    refuted(_, _, Room, _), iHave(Room), writeLog(['R3']).
+refute(Suspect, _W, _R, _A, Suspect) :- iHave(Suspect), writeLog(['R4']).
+refute(_S, Weapon, _R, _A, Weapon) :- iHave(Weapon), writeLog(['R5']).
+refute(_S, _W, Room, _A, Room) :- iHave(Room), writeLog(['R6']).
 
 
 % Make accusation succeeds if I know the three items
@@ -241,7 +270,7 @@ makeAccusation(Suspect, Weapon, Room, Me) :-
   iAm(Me), 
   isSuspect(Suspect), 
   isWeapon(Weapon),
-  isRoom(Room).
+  isRoom(Room), writeLog(['MA1']).
 
 
 % Make a suggestion - moves me and the suspect to the new room
@@ -251,27 +280,48 @@ makeSuggestion(Suspect, Weapon, Room, Me) :-
   pickRoom(Room), 
   iAm(Me),
   moveTo(Suspect, Room),
-  moveTo(Me, Room).
+  moveTo(Me, Room), writeLog(['MS1']).
 
+% Get a random element from a list 
+randomElement(List, RandomElement) :-
+     length(List, Len), Len > 0,
+     Index is random(Len),
+     nth0(Index, List, RandomElement), writeLog(['RandomElement']).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % Suspect selection rules
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % If I know who it is, use that suspect
-pickSuspect(S) :- isSuspect(S).
+pickSuspect(S) :- isSuspect(S), writeLog(['PS1']).
 
-% Pick a suspect from the possible suspects
-pickSuspect(Suspect) :- possibleSuspects(S), member(Suspect, S).
+% Pick a random suspect from the possible suspects
+pickSuspect(S) :- possibleSuspects(SusList), randomElement(SusList, S), writeLog(['PS2']).
 
+% Pick a random suspect from any of the suspects
+pickSuspect(S) :- setof(Sus, suspect(Sus), Ss),
+     randomElement(Ss, S), writeLog(['PS3']).
+
+% Can pick based on probability?
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % Weapon selection rules
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % If I know what they used, use that weapon 
-pickWeapon(W) :- isWeapon(W).
+pickWeapon(W) :- isWeapon(W), writeLog(['PW1']).
 
-% Pick a weapon from the possible weapons
-pickWeapon(Weapon) :- possibleWeapons(W), member(Weapon, W).
+
+%*****
+% Pick the weapon with the highest probability of being in the answer
+% The weapon that the highest amount of people can't have it
+% Get the number of people that can't have the weapon
+    
+
+% Pick a random weapon from the possible weapons
+pickWeapon(W) :- possibleWeapons(WeapList), randomElement(WeapList, W), writeLog(['PW2']).
+
+% Pick a random weapon from any of the weapons
+pickWeapon(W) :- setof(Weap, weapon(Weap), Ws),
+     randomElement(Ws, W), writeLog(['PW3']).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -279,9 +329,100 @@ pickWeapon(Weapon) :- possibleWeapons(W), member(Weapon, W).
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % Pick a room next to the room I'm in that is still possible
 pickRoom(NewRoom) :- iAm(Me), isIn(Me, Room), adjacent(Room, NewRoom),
-                     possibleRooms(P), member(NewRoom, P).
+                     possibleRooms(P), member(NewRoom, P), writeLog(['PickRoom1']).
+
+% If none of the rooms around me are possible then choose a room that is closest 
+% to another possible room
+% Get a list of all possible rooms and their distances 
+% Pick a random? one from the list 
+% Find path to any of the unknown nodes 
+% Not sure if this works well or not
+pickRoom(NewRoom) :- writeLog(['Trying PR2']), iAm(Me), isIn(Me, MyRoom), possibleRooms(Good), setof(D, G^(member(G, Good), distance(MyRoom, G, D)), [Dist | _]),
+  setof(R, distance(MyRoom, R, Dist), [ DestRoom | _]), nextRoom(MyRoom, DestRoom, NewRoom), writeLog(['Pick Room', NewRoom]).
+
 % Pick a room next to the room I'm in
-pickRoom(NewRoom) :- iAm(Me), isIn(Me, Room), adjacent(Room, NewRoom).
+pickRoom(NewRoom) :- iAm(Me), isIn(Me, Room), adjacent(Room, NewRoom), writeLog(['PickRoom3']).
+
+% Get the number of players that can't have a card
+numCantHave(Card, Num) :-
+     findall(Player, suspect(Player), cantHave(Player, Card), WhoCantHaveCard), list_to_set(WhoCantHaveCard, SetWhoCantHaveCard),
+     length(SetWhoCantHaveCard, Num), writeLog(['numCantHave', Card, ' ', WhoCantHaveCard]).
+
+%******
+% Facts about which rooms are next to which rooms in Clue
+nextTo(hall, study).
+nextTo(study, library).
+nextTo(library, billiardRoom).
+nextTo(billiardRoom, conservatory).
+nextTo(conservatory, ballroom).
+nextTo(ballroom, kitchen).
+nextTo(kitchen, diningRoom).
+nextTo(diningRoom, lounge).
+nextTo(lounge, hall).
+nextTo(lounge, conservatory).
+nextTo(study, kitchen).
+
+% The actual relation we will use is adjacent() which is defined by noting
+% that the nextTo() relation is really bi-directional
+adjacent(X,Y) :- nextTo(X,Y).
+adjacent(X,Y) :- nextTo(Y,X).
+
+
+%%%%%%%%%%%%%%%%
+% nextRoom() uses breadth-first search below to find a path from X to Y 
+% and returns the next room we should move to in order to move from X to Y.
+% The nextToLastItem() relation helps pick off the next room to visit from 
+% the path list.  The distance() relation returns the distance between rooms
+%%%%%%%%%%%%%%%
+
+% Find a path from X to Y as a list [Y..X] and our next room is 
+% the next-to-last item in that path list - note that this works
+% correctly for immediately adjacent rooms as well since the list will 
+% have two elements
+nextRoom(Current, Destination, Next) :-
+        breadthFirst([[Current] | Temp] - Temp, Path, Destination),
+        nextToLastItem(Path, Next).
+
+% Distance is length of the path - 1
+distance(Current, Destination, Length) :-
+        breadthFirst([[Current] | Temp] - Temp, Path, Destination),
+        length(Path, L), Length is L-1, writeLog(['Distance']).
+
+% Given a list, pick off the next-to-last item
+nextToLastItem([Item | [_|[]]], Item).
+nextToLastItem([_ | Tail], Item) :- nextToLastItem(Tail, Item).
+
+
+
+%%%%%%%%%%%%%%%%%%%
+% Breadth-first search
+%
+% General breadth-first search returns shortest path between two nodes
+% where distances are uniform.  
+%%%%%%%%%%%%%%%%%%%%
+
+% If the head of the list for the path is the goal node, then we have a path and
+% are done!
+breadthFirst([[Node | Path]|_] - _, [Node | Path], Goal) :- Goal = Node.
+
+% Otherwise we need to extend the path with adjacent nodes
+breadthFirst([Path | Paths] - Z, Solution, Goal) :-
+        extend(Path, NewPaths),
+        append(NewPaths, Z1, Z),
+        Paths \== Z1,
+        breadthFirst(Paths - Z1, Solution, Goal).
+        
+% Paths are extended with nodes that are adjacent
+extend([Node | Path], NewPaths) :- 
+        bagof([NewNode, Node | Path],
+              (adjacent(Node, NewNode), 
+                 \+ member(NewNode, [Node | Path])), % no cycles
+              NewPaths), !.             % if we got this far, don't try others
+% Otherwise, we are at a dead end - no way to extend the path so we succeed but
+% return an emtpy list of new paths
+extend(_,[]).
+
+%******
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -324,9 +465,26 @@ possibleRooms(Possible) :-
 
 % One way to prove that I know a part of the solution is if there is 
 % only one card left in the set of possible objects 
-isSuspect(Suspect) :- possibleSuspects([Suspect]).
-isWeapon(Weapon) :- possibleWeapons([Weapon]).
-isRoom(Room) :- possibleRooms([Room]).
+isSuspect(S) :- possibleSuspects([S]), writeLog(['isSus1', S]),
+    addNewFact(isSusAnswer(S)).
+
+% if we know that the number of players that can't have a suspect is the number
+% of suspects, then we know that the suspect must be the answer
+isSuspect(S) :- suspect(S), numCantHave(S, Num), allSuspects(AllS), 
+    length(AllS, Num), writeLog(['isSus2', S]), addNewFact(isSusAnswer(S)).
+
+isWeapon(W) :- possibleWeapons([W]), writeLog(['isWeap1', W]),
+    addNewFact(isWeapAnswer(W)).
+
+isWeapon(W) :- weapon(W), numCantHave(W, Num), allWeapons(AllW), 
+    length(AllW, Num), writeLog(['isWeap2', W]), addNewFact(isWeapAnswer(W)).
+
+isRoom(R) :- possibleRooms([R]), writeLog(['isRoom1', R]), 
+    addNewFact(isRoomAnswer(R)), addNewFact(isRoomAnswer(R)).
+
+isRoom(R) :- room(R), numCantHave(R, Num), allRooms(AllR), 
+    length(AllR, Num), writeLog(['isRoom2', R]),
+    addNewFact(isRoomAnswer(R)).
 
 % Ways to prove a player has a card
 % 1) I have the card
@@ -342,7 +500,7 @@ hasCard(Player, Card) :- deducedHasCard(Player, Card).
 
 % Ways to prove a person does NOT have a card - the key to finding solution!
 % 1) non-active suspects can not have any cards
-cantHave(Player, _Card) :- suspect(Player), \+ activePlayer(Player).
+cantHave(Player, Card) :- suspect(Player), \+ activePlayer(Player).
 
 % 2) If I am the player and do not have the card
 cantHave(Player, Card) :- iAm(Player), \+ iHave(Card).
@@ -351,9 +509,9 @@ cantHave(Player, Card) :- iAm(Player), \+ iHave(Card).
 cantHave(Player, Card) :- hasCard(Player2, Card), Player2 \== Player.
 
 % 4) If the solution has the card
-cantHave(_Player, Card) :- isSuspect(Card).
-cantHave(_Player, Card) :- isWeapon(Card).
-cantHave(_Player, Card) :- isRoom(Card).
+cantHave(Player, Card) :- isSusAnswer(Card).
+cantHave(Player, Card) :- isWeapAnswer(Card).
+cantHave(Player, Card) :- isRoomAnswer(Card).
 
 % 5) If a suggestion was made with the card and they didn't refute it
 cantHave(Player, Card) :- suggested(Card, Weapon, Room, Player), 
@@ -362,7 +520,7 @@ cantHave(Player, Card) :- suggested(Suspect, Card, Room, Player),
   didntRefute(Suspect, Card, Room, Player).
 cantHave(Player, Card) :- suggested(Suspect, Weapon, Card, Player), 
   didntRefute(Suspect, Weapon, Card, Player).
-
+  
 
 % Turn logging on/off
 %writeLog(_). % not logging, just succeed.
